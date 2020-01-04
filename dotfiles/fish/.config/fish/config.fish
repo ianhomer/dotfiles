b891978e28c3f7c18305021957eff2139f1296b4
Ian Homer <code@ianhomer.com>
2020-01-04 16:31:51 +0000

    Lint

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
 status --is-interactive; and source (rbenv init - | psub)
 
-echo "...Loaded ~/.config/fish/conf.fish"
-
 set PATH ~/.dotfiles/bin $PATH
 
 # Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
 function __fish_describe_command; end
+
+echo "...Loaded ~/.config/fish/conf.fish"
 
