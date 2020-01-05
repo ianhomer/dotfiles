cf414d4c12fe6adefa2f8706ede92e83e000f169
Ian Homer <ian@purplepip.com>
2020-01-05 13:55:10 +0000

    Set personal git config

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -1,9 +1,11 @@
 # Shortcuts
 
 alias g="git"
-alias gs="git status"
-alias ga="git add"
-alias gc="git commit"
+alias gits="git status"
+alias gita="git add"
+alias gitc="git commit"
+alias git-whoami="git config user.name ; git config user.email"
+alias git-config-personal="git config user.email ian@purplepip.com"
 
 echo "...Loaded ~/.config/fish/aliases.fish"
 
