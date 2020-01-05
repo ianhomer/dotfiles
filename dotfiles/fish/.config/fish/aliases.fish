6182d21df0ea8d6abd9d049a0c6acf9d39696ec4
Ian Homer <code@ianhomer.com>
2020-01-05 08:40:48 +0000

    Add fish aliases

---
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -0,0 +1,9 @@
+# Shortcuts
+
+alias g="git"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+
+echo "...Loaded ~/.config/fish/aliases.fish"
+
--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,6 +2,8 @@ status --is-interactive; and source (rbenv init - | psub)
 
 set PATH ~/.dotfiles/bin $PATH
 
+source ~/.config/fish/aliases.fish
+
 # Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
 function __fish_describe_command; end
 
