c7be66cf04d9fad4a79bce48f147adcf8c9541c0
Ian Homer <code@ianhomer.com>
2020-01-05 14:36:58 +0000

    Add gitp alias

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -4,6 +4,7 @@ alias g="git"
 alias gits="git status"
 alias gita="git add"
 alias gitc="git commit"
+alias gitp="git push"
 alias git-whoami="git config user.name ; git config user.email"
 alias git-config-personal="git config user.e:mail ian@purplepip.com"
 
