c42d5a94ee3b659e070e0d9655b5fb997f97c534
Ian Homer <ian@purplepip.com>
2020-01-05 14:04:09 +0000

    Add git reset ssh key function

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -5,7 +5,7 @@ alias gits="git status"
 alias gita="git add"
 alias gitc="git commit"
 alias git-whoami="git config user.name ; git config user.email"
-alias git-config-personal="git config user.email ian@purplepip.com"
+alias git-config-personal="git config user.e:mail ian@purplepip.com"
 
 echo "...Loaded ~/.config/fish/aliases.fish"
 
