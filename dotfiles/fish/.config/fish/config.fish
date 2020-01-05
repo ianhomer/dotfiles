d23b79319a6328784620a9b5b64c7b6608df1456
Ian Homer <ian@purplepip.com>
2020-01-05 14:46:24 +0000

    Only load functions and echo in interactive fish shell

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -2,11 +2,12 @@ status --is-interactive; and source (rbenv init - | psub)
 
 set PATH ~/.dotfiles/bin $PATH
 
-source ~/.config/fish/aliases.fish
-source ~/.config/fish/functions.fish
+status --is-interactive; and
+  source ~/.config/fish/aliases.fish
+  source ~/.config/fish/functions.fish
 
-# Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
-function __fish_describe_command; end
+  # Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
+  function __fish_describe_command; end
 
-echo "... Loaded ~/.cnfig/fish/config.fish"
-
+  echo "... Loaded ~/.cnfig/fish/config.fish"
+end
