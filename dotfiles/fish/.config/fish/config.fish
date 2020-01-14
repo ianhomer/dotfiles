7a26b6deb4e761c02185f62911374166370bf65d
Ian Homer <code@ianhomer.com>
2020-01-14 08:51:29 +0000

    Improve inputrc settings

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -9,5 +9,7 @@ if status --is-interactive
   # Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
   function __fish_describe_command; end
 
+  # Use vi key bindings
+  fish_vi_key_bindings
   echo "... Loaded ~/.cnfig/fish/config.fish"
 end
