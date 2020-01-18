603faa5a787bbd7c301cbfb026cfde78057bb67e
Ian Homer <ian@purplepip.com>
2020-01-18 07:50:40 +0000

    Remove date time from fish right prompt

--- dotfiles/fish/.config/fish/functions.fish
+++ dotfiles/fish/.config/fish/functions.fish
@@ -1,5 +1,9 @@
 source ~/Library/Preferences/org.dystroy.broot/launcher/fish/br 
 
+function fish_right_prompt
+  #intentionally left blank
+end
+ 
 function git-reset-ssh-key
   ssh-add -D
   ssh-add -l
