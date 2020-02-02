b447583ff0f35c0bda9e65a73bd2477f5da66cf1
Ian Homer <ian@purplepip.com>
2020-02-02 09:19:25 +0000

    Tweak comments

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -9,10 +9,12 @@ if status --is-interactive
   source ~/.config/fish/aliases.fish
   source ~/.config/fish/functions.fish
 
-  # Work around for OSX issue causing slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
+  # Work around for OSX issue causing 
+  # slow autocompletion - https://github.com/fish-shell/fish-shell/issues/6270
   function __fish_describe_command; end
 
-  # Use vi key bindings
+  # use vi key bindings, go back to default key bindings 
+  # with fish_default_key_bindings 
   fish_vi_key_bindings
   echo "... Loaded ~/.cnfig/fish/config.fish"
 
