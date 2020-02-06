bffe5cfca5c81d39e7e0d9b7686c07eb9dc93da2
Ian Homer <ian@purplepip.com>
2020-02-06 20:43:20 +0000

    Add more git aliases

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -1,9 +1,13 @@
 # Shortcuts
 
 alias g="git"
+alias gs="git s"
 alias gits="git status"
+alias gd="git diff"
+alias gd~="git diff HEAD~"
 alias gita="git add"
 alias gitc="git commit"
+alias gp="git push"
 alias gitp="git push"
 alias git-whoami="git config user.name ; git config user.email"
 alias git-config-personal="git config user.e:mail ian@purplepip.com"
