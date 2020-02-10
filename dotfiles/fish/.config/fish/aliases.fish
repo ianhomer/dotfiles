5c3d556bb9631e56e4073d48a50eb8c613c7638b
Ian Homer <ian@purplepip.com>
2020-02-10 21:00:50 +0000

    Lastpass CLI and disable Ctrl-arrows

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -12,5 +12,8 @@ alias git-whoami="git config user.name ; git config user.email"
 alias git-config-personal="git config user.e:mail ian@purplepip.com"
 
 alias draw="open -a /Applications/draw.io.app/"
+
+alias pass="lpass show --password -c"
+alias pass-ls"lpass ls"
 echo "... Loaded ~/.config/fish/aliases.fish"
 
