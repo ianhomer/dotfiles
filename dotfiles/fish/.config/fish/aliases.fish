5562d536fd09ec088ff2d693ce92d4a39b93dce2
Ian Homer <ian@purplepip.com>
2020-02-12 16:51:48 +0000

    correct alias

--- dotfiles/fish/.config/fish/aliases.fish
+++ dotfiles/fish/.config/fish/aliases.fish
@@ -13,7 +13,6 @@ alias git-whoami="git config user.name ; git config user.email"
 alias git-config-personal="git config user.e:mail ian@purplepip.com"
 
 # tmux
-
 alias t="tmux"
 alias ta="t a -t"
 alias tls="t ls"
@@ -22,6 +21,6 @@ alias tn="t new -t"
 alias draw="open -a /Applications/draw.io.app/"
 
 alias pass="lpass show --password -c"
-alias pass-ls"lpass ls"
+alias pass-ls="lpass ls"
 echo "... Loaded ~/.config/fish/aliases.fish"
 
