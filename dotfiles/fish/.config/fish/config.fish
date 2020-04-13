85aa1dd32199ed10fc30c60bbec792db4d7473ff
Ian Homer <ian@purplepip.com>
2020-04-13 13:54:51 +0100

    Add timings to fish start up

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -1,8 +1,17 @@
 set CONFIG_LOG_LEVEL 0
 status --is-login; and set CONFIG_LOG_LEVEL 1
-#set CONFIG_LOG_LEVEL 2
+set CONFIG_LOG_LEVEL 3
 
-[ {$CONFIG_LOG_LEVEL} -gt 1 ] ;and echo "START : PATH = $PATH"
+if [ {$CONFIG_LOG_LEVEL} -gt 1 ]
+  set START_DATE (gdate +%s%3N)
+  echo "START : $START_DATE"
+  echo "PATH  : $PATH"
+
+  function time-me
+    set DATE (gdate +%s%3N)
+    printf "    TIME : %20s : %s\n" $argv[1] (expr $DATE - $START_DATE)
+  end
+end
 if [ {$CONFIG_LOG_LEVEL} -gt 2 ]
   status --is-interactive; and echo "... INTERACTIVE shell"
   status --is-login; and echo "... LOGIN shell"
@@ -14,22 +23,23 @@ if status --is-login
   # jenv set up
   #
 
-  set PATH $HOME/.jenv/bin $PATH
   source (jenv init -|psub)
 
   #
   # rbenv set up
   #
-  source (rbenv init - | psub)
-  set PATH ~/.dotfiles/bin $PATH
+  source (rbenv init -|psub)
 
   # Point OMF state to dotfiles
   set -g OMF_CONFIG ~/.dotfiles/config/omf
 end
 
 if status --is-interactive
+  time-me "BEFORE aliases"
   source ~/.config/fish/aliases.fish
   source ~/.config/fish/functions.fish
+  set DATE (gdate +%s%3N)
+  time-me "AFTER functions"
 
   #
   # Work around for OSX issue causing
@@ -68,6 +78,9 @@ if status --is-login
   set -x GPG_TTY (tty)
 end
 
-[ {$CONFIG_LOG_LEVEL} -gt 1 ] ;and echo "END   : PATH = $PATH"
+if [ {$CONFIG_LOG_LEVEL} -gt 1 ]
+  time-me "END"
+  echo "PATH = $PATH"
+end
 [ {$CONFIG_LOG_LEVEL} -gt 0 ] ;and echo "... Loaded ~/.config/fish/config.fish"
 
