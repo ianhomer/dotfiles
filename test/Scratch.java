1eb007678c21d09844fbaeb837cae2bc64256589
Ian Homer <ian.homer@gmail.com>
2020-04-25 13:29:33 +0100

    Python / Java scratch pads

--- test/Scratch.java
+++ test/Scratch.java
@@ -2,4 +2,12 @@ public class Scratch {
   public String foo(String bar) {
     return bar;
   }
+
+  public String hello(String bar) {
+    return "Hello, " + bar + "!";
+  }
+
+  public static void main(String[] args) {
+    System.out.println(new Scratch().hello("world"));
+  }
 }
---
+++ test/scratch.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+#
+# Python scratch pad
+#
+
+def foo(bar):
+  print(f"foo executed with {bar}")
+
+bar = "hello";
+foo(bar)
