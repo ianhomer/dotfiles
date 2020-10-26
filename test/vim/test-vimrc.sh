360a8e349c349ac717d6f220618890169a696362
Ian Homer <ian.homer@gmail.com>
2020-10-26 13:59:35 +0000

    Simple test

--- test/vim/test-vimrc.sh
+++ test/vim/test-vimrc.sh
@@ -2,13 +2,13 @@
 
 echo "Running tests"
 
-PLUGIN_DIR=~/.config/nvim/plugged
+#PLUGIN_DIR=~/.config/nvim/plugged
 
-vim -Nu <(cat << VIMRC
-filetype off
-set rtp+=$PLUGIN_DIR/vader.vim
-set rtp+=.
-set rtp+=after
-filetype plugin indent on
-syntax enable
-VIMRC) -c 'Vader! *.vader' > /dev/null
+#vim -Nu <(cat << VIMRC
+#filetype off
+#set rtp+=$PLUGIN_DIR/vader.vim
+#set rtp+=.
+#set rtp+=after
+#filetype plugin indent on
+#syntax enable
+#VIMRC) -c 'Vader! *.vader' > /dev/null
