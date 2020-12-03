bf3ab589d87d33befaae8f45f4be2e512de2cd5d
Ian Homer <ian@purplepip.com>
2020-12-03 08:12:06 +0000

    Fix gd alias

--- dotfiles/sh/.config/sh/aliases.sh
+++ dotfiles/sh/.config/sh/aliases.sh
@@ -6,7 +6,7 @@ alias do="todo"
 alias d="docker"
 alias push="git push"
 alias gs="git status"
-alias gd~="git diff HEAD~"
+alias gd="git diff HEAD~"
 
 # Attach to tmux session if exists, otherwise new
 alias tm="tmux attach || tmux new"
