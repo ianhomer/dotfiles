f075a0db0285efe3d8806fe6768ff903ee5a2fb6
Ian Homer <ian@purplepip.com>
2021-03-15 21:43:10 +0000

    Create thingity library

---
+++ bin/lib/thingity.py
@@ -0,0 +1,13 @@
+import configparser
+from pathlib import Path
+from datetime import datetime
+
+config = configparser.ConfigParser()
+config.read(str(Path.home()) + "/.config/dotme/shim.ini")
+THINGS_DIR = config["DEFAULT"]["THINGS_DIR"]
+MY_NOTES = config["DEFAULT"]["MY_NOTES"]
+
+
+def getTodayLog(now=datetime.now()):
+    today = now.strftime("%m%d")
+    return f"{THINGS_DIR}/{MY_NOTES}/stream/{today}.md"
