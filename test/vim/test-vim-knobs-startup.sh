027ff26ddf6d04324c74492bdbbaee09c56c1101
Ian Homer <ian@purplepip.com>
2021-04-08 09:04:06 +0100

    Knob startup script

--- test/vim/test-vim-knobs-startup.sh
+++ test/vim/test-vim-knobs-startup.sh
@@ -1,11 +1,12 @@
-#!/usr/bin/env fish
+#!/usr/bin/env bash
 
-set fish_trace on
-
+#set fish_trace on
+set -x
 time VIM_KNOB=0 nvim --noplugin -u NONE +q
 time VIM_KNOB=0 nvim --noplugin +q
 time VIM_KNOB=0 nvim +q
-set -e fish_trace
+set +x
+#set -e fish_trace
 
 echo "KNOB 1"
 time VIM_KNOB=1 nvim +q
@@ -17,4 +18,5 @@ echo "KNOB 4"
 time VIM_KNOB=4 nvim +q
 echo "KNOB 5"
 time VIM_KNOB=5 nvim +q
+
 
