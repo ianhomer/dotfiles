cadf915f7d3cfd4cf3123fb238853d986a6bd3d5
Ian Homer <ian.homer@gmail.com>
2021-04-11 17:14:53 +0100

    Hidden files for find

--- dotfiles/nvim/.config/nvim/lua/config/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/config/telescope.lua
@@ -15,3 +15,6 @@ require("telescope").setup {
 
 local opt = {noremap = true, silent = true}
 vim.api.nvim_set_keymap("n", " t", [[<Cmd>Telescope<CR>]], opt)
+vim.api.nvim_set_keymap("n", " tb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], opt)
+vim.api.nvim_set_keymap("n", " tf", [[<Cmd>lua require('telescope.builtin').find_files({hidden = true})<CR>]], opt)
+vim.api.nvim_set_keymap("n", " th", [[<Cmd>lua require('telescope.builtin').oldfiles()<CR>]], opt)
