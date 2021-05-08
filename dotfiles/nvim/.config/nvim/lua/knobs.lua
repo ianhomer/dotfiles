58a3c77b046b1f6ac750e12ce6c7845608efa6de
Ian Homer <ian@purplepip.com>
2021-05-08 08:45:44 +0100

    Support nvim in package name at end for knob match

--- dotfiles/nvim/.config/nvim/lua/knobs.lua
+++ dotfiles/nvim/.config/nvim/lua/knobs.lua
@@ -1,5 +1,6 @@
 local M = {}
 local KNOB_VIM_RE = "^[%w-]+/n?vim%-([%w-]+)"
+local KNOB_VIM_AFTER_RE = "^[%w-]+/([%w-]+)-n?vim"
 local KNOB_RE = "^[%w-]+/([%w-]+)"
 local REPO_RE = "^[%w-]+/([%w-_.]+)$"
 local cmd = vim.api.nvim_command
@@ -11,7 +12,7 @@ function M.has(knob)
 end
 
 function knobFromPackage(package)
-    return (package:match(KNOB_VIM_RE) or package:match(KNOB_RE)):gsub("-", "_")
+    return (package:match(KNOB_VIM_RE) or package:match(KNOB_VIM_AFTER_RE) or package:match(KNOB_RE)):gsub("-", "_")
 end
 
 function M.useif(use, disableIf)
@@ -24,14 +25,14 @@ function M.useif(use, disableIf)
         knob = knobFromPackage(package)
         local knobVariable = "knob_" .. knob
         if vim.g["knobs_levels"][knob] ~= nil then
-            -- print(package .. ":" .. knob .. ":" .. tostring(vim.g[knobVariable]))
+            print(package .. ":" .. knob .. ":" .. tostring(vim.g[knobVariable]))
             if disableIf then
                 args.disable = (vim.g[knobVariable] or 0) == 0
             else
                 args.cond = "vim.g[knobVariable]"
             end
         else
-            -- print(package .. ":" .. knob .. " does not exist")
+            print(package .. ":" .. knob .. " does not exist")
         end
         use(args)
     end
