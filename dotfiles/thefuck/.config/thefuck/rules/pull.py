4f1c699a90d757bdf25ee87e322ad1cbca392243
Ian Homer <ian@purplepip.com>
2021-05-29 16:23:29 +0100

    Python flake

--- dotfiles/thefuck/.config/thefuck/rules/pull.py
+++ dotfiles/thefuck/.config/thefuck/rules/pull.py
@@ -1,7 +1,9 @@
 def match(command):
-    return 'pull' in command.script and 'set-upstream' in command.output
+    return "pull" in command.script and "set-upstream" in command.output
+
 
 def get_new_command(command):
     return "git push -u && git pull"
+
 
 priority = 1
