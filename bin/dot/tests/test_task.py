388783a247feed293a748efaa253a1942c1a23f2
Ian Homer <ian@purplepip.com>
2021-06-01 12:23:54 +0100

    Dynamic field attributes

--- bin/dot/tests/test_task.py
+++ bin/dot/tests/test_task.py
@@ -15,21 +15,11 @@ def file_with_tasks(file, task):
     return dict(task=Task(file + ":" + task))
 
 
-@then(parsers.parse("the context is {context}"))
-def task_should_have_context(tasks, context):
-    assert tasks["task"].context == context
+@then(parsers.parse("the {field} is {value}"))
+def task_should_have_field_value(tasks, field, value):
+    assert getattr(tasks["task"], field) == value
 
 
-@then(parsers.parse("the subject is {subject}"))
-def task_should_have_subject(tasks, subject):
-    assert tasks["task"].subject == subject
-
-
-@then(parsers.parse("the date is not set"))
-def task_should_not_have_date(tasks):
-    assert tasks["task"].date is None
-
-
-@then(parsers.parse("the file is {file}"))
-def task_should_have_file(tasks, file):
-    assert tasks["task"].file == file
+@then(parsers.parse("the {field} is not set"))
+def task_should_not_have_field_set(tasks, field):
+    assert getattr(tasks["task"], field) is None
