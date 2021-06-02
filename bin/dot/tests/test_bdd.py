01fa7e81d45967c37b0adaaf685eefb6c49ed8cb
Ian Homer <ian@purplepip.com>
2021-06-02 08:40:54 +0100

    Lint

--- bin/dot/tests/test_bdd.py
+++ bin/dot/tests/test_bdd.py
@@ -1,4 +1,4 @@
-from pytest_bdd import scenarios, given, when, then, parsers
+from pytest_bdd import scenarios, given, then, parsers
 
 from dot.task import Task
 from dot.humanDate import HumanDate
