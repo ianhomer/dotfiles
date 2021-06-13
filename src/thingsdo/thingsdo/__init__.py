22b2cc137697801ac30d180d6f1fad9021a21075
Ian Homer <ian@purplepip.com>
2021-06-13 11:56:54 +0100

    Register palette in module

--- src/thingsdo/thingsdo/__init__.py
+++ src/thingsdo/thingsdo/__init__.py
@@ -1,4 +1,5 @@
+from .contextFilter import ContextFilter
 from .humanDate import HumanDate
 from .humanTime import HumanTime
+from .palette import Palette
 from .task import Task
-from .contextFilter import ContextFilter
