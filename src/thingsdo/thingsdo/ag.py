253cfb0cbde63f2231f2eae6413a96a83e8b1af5
Ian Homer <ian@purplepip.com>
2021-07-03 08:52:14 +0100

    Ignore archive todos by default

--- src/thingsdo/thingsdo/ag.py
+++ src/thingsdo/thingsdo/ag.py
@@ -1,8 +1,15 @@
 class Ag:
     def __init__(self, path, justArchive=False, withArchive=False):
         self.path = path
-        self.justArchive = justArchive
-        self.withArchive = withArchive
+        self.archiveParts = self.createArchiveParts(justArchive, withArchive)
+
+    def createArchiveParts(self, justArchive, withArchive):
+        if justArchive:
+            return ["--file-search-regex", "\\/archive\\/"]
+        elif withArchive:
+            return []
+        else:
+            return ["--ignore-dir", "archive"]
 
     def parts(self, pattern, options):
-        return ["ag"] + options + [pattern, self.path]
+        return ["ag"] + options + self.archiveParts + [pattern, self.path]
