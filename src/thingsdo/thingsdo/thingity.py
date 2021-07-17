2333a6bc65a69c2057f6bcd8ed63794619f6931d
Ian Homer <ian@purplepip.com>
2021-07-17 22:05:31 +0100

    Use environment in thingity

--- src/thingsdo/thingsdo/thingity.py
+++ src/thingsdo/thingsdo/thingity.py
@@ -1,25 +1,21 @@
-import configparser
 import datetime
 import glob
 import os
 import subprocess
 import time
-from pathlib import Path
-from . import runner, Signal, Thing
+from . import Environment, runner, Signal, Thing
 
-config = configparser.ConfigParser()
-config.read(str(Path.home()) + "/.config/dotme/shim.ini")
-THINGS_DIR = config["DEFAULT"]["THINGS_DIR"]
-MY_NOTES = config["DEFAULT"]["MY_NOTES"]
-MY_NOTES_DIR = THINGS_DIR + "/" + MY_NOTES
-shouldSynkFile = str(Path.home()) + "/.config/dotme/should-run/last-run-git-synk-things"
+environment = Environment()
+MY_NOTES = environment.config["MY_NOTES"]
+MY_NOTES_DIR = environment.directory + "/" + MY_NOTES
+shouldSynkFile = environment.home + "/.config/dotme/should-run/last-run-git-synk-things"
 
 
 def synk(force, justMyNotes=False):
     if not force and not runner.should(shouldSynkFile):
         return
     if force:
-        dir = MY_NOTES_DIR if justMyNotes else THINGS_DIR
+        dir = MY_NOTES_DIR if justMyNotes else environment.directory
         subprocess.run(["git", "synk"], cwd=dir)
         runner.has(shouldSynkFile)
         time.sleep(1)
@@ -32,18 +28,18 @@ def synk(force, justMyNotes=False):
 
 def getTodayLog(now=datetime.datetime.now()):
     today = now.strftime("%m%d")
-    return f"{THINGS_DIR}/{MY_NOTES}/stream/{today}.md"
+    return f"{environment.directory}/{MY_NOTES}/stream/{today}.md"
 
 
 def getPath(name):
-    return f"{THINGS_DIR}/{MY_NOTES}/stream/{name}.md"
+    return f"{environment.directory}/{MY_NOTES}/stream/{name}.md"
 
 
 def lint(fix=False):
     signals = []
-    for filename in glob.iglob(f"{THINGS_DIR}/**/*.md", recursive=True):
+    for filename in glob.iglob(f"{environment.directory}/**/*.md", recursive=True):
         try:
-            thing = Thing(filename, root=THINGS_DIR)
+            thing = Thing(filename, root=environment.directory)
             if not thing.normal:
                 thing.normalise(fix)
         except Exception as exception:
