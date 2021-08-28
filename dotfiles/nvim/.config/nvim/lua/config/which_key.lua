511bfb41e5ddb45ae4c28bc065de806be9e3545b
Ian Homer <ian@purplepip.com>
2021-08-28 08:00:01 +0100

    Telescope find hidden files

--- dotfiles/nvim/.config/nvim/lua/config/which_key.lua
+++ dotfiles/nvim/.config/nvim/lua/config/which_key.lua
@@ -1,11 +1,8 @@
 local wk = require("which-key")
 
 wk.setup {
-  plugins = {
-    marks = true
-  }
 }
 
 wk.register({
-  f = { "<cmd>Telescope find_files<cr>", "Find File" }
+  f = { "<cmd>Telescope find_files hidden=true<cr>", "Find File" }
 }, { prefix = "<leader>" })
