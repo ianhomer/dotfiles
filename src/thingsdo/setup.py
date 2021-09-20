01084dc082bd5b25d56d504aa192bc0cd240d994
Ian Homer <ian@purplepip.com>
2021-09-20 10:17:03 +0100

    Linting thingsdo set up and package update

--- src/thingsdo/setup.py
+++ src/thingsdo/setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup
+
 setup(
-    name='thingsdo',
-    version='0.0.1',
-    description='Todo and note management',
-    packages=['thingsdo'],
+    name="thingsdo",
+    version="0.0.1",
+    description="Todo and note management",
+    packages=["thingsdo"],
     entry_points={
-        'console_scripts': [
-            'todo=thingsdo.cli.todo:run',
-            'thing=thingsdo.cli.thing:run',
-            'things=thingsdo.cli.things:run',
-            'things-search=thingsdo.cli.search:run',
-            'things-with-modified=thingsdo.cli.withModified:run',
+        "console_scripts": [
+            "todo=thingsdo.cli.todo:run",
+            "thing=thingsdo.cli.thing:run",
+            "things=thingsdo.cli.things:run",
+            "things-search=thingsdo.cli.search:run",
+            "things-with-modified=thingsdo.cli.withModified:run",
         ]
-    }
+    },
 )
