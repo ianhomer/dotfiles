40dde0b131657b576c0fd4fb08648d19c378c20b
Ian Homer <ian@purplepip.com>
2021-10-04 07:49:16 +0100

    Disable trouble telescope

--- dotfiles/nvim/.config/nvim/lua/config/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/config/telescope.lua
@@ -1,5 +1,5 @@
 local actions = require("telescope.actions")
-local trouble = require("trouble.providers.telescope")
+-- local trouble = require("trouble.providers.telescope")
 local telescope = require("telescope")
 
 telescope.setup {
@@ -14,10 +14,10 @@ telescope.setup {
             "--smart-case",
             "--hidden"
         },
-        mappings = {
-            i = {["<c-t>"] = trouble.open_with_trouble},
-            n = {["<c-t>"] = trouble.open_with_trouble}
-        },
+        -- mappings = {
+        --    i = {["<c-t>"] = trouble.open_with_trouble},
+        --    n = {["<c-t>"] = trouble.open_with_trouble}
+        --},
         path_display = {"shorten"}
     }
 }
