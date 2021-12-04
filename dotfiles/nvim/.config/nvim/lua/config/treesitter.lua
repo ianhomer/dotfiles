4bebb1f119059499aae689b2583507099b0f5818
Ian Homer <ian.homer@gmail.com>
2021-12-04 09:11:43 +0000

    Add incremental_search support

--- dotfiles/nvim/.config/nvim/lua/config/treesitter.lua
+++ dotfiles/nvim/.config/nvim/lua/config/treesitter.lua
@@ -13,10 +13,19 @@ treesitter.setup {
         "scss",
         "tsx",
         "typescript",
-        "yaml",
+        "yaml"
     },
     highlight = {
         enable = true,
         use_languagetree = true
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "<CR>",
+            scope_incremental = "<CR>",
+            node_incremental = '<TAB>',
+            node_decremental = '<S-TAB>'
+        }
     }
 }
