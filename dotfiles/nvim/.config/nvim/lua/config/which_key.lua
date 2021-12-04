920b6bfa18a42677cb0849efed313673a622046b
Ian Homer <ian.homer@gmail.com>
2021-12-04 18:33:51 +0000

    Explore other telescope finders

--- dotfiles/nvim/.config/nvim/lua/config/which_key.lua
+++ dotfiles/nvim/.config/nvim/lua/config/which_key.lua
@@ -5,18 +5,39 @@ wk.setup {}
 
 local map = {}
 if vim.g.knob_telescope then
-    map["tb"] = {"<cmd>Telescope git_bcommits<cr>", "Buffer commits"}
-    map["tc"] = {"<cmd>Telescope commands<cr>", "Recent commands"}
-    map["td"] = {"<cmd>Telescope lsp_document_symbols<cr>", "LSP document symbols"}
+    -- Common Searches
     map["f"] = {"<cmd>Telescope find_files hidden=true<cr>", "Find File"}
     map["s"] = {"<cmd>Telescope live_grep<cr>", "Search"}
-    map["tt"] = {"<cmd>Telescope<cr>", "Telescope"}
+    map["<space>"] = {"<cmd>Telescope buffers<cr>", "Buffers"}
+
+    -- Alternatives
     map["tS"] = {"<cmd>Telescope live_grep hidden=true<cr>", "Search"}
+
+    -- File
+    map["tF"] = {"<cmd>Telescope file_browser<cr>", "File Browser"}
+
+    -- Pickers
+    map["tt"] = {"<cmd>Telescope<cr>", "Telescope"}
     map["th"] = {"<cmd>Telescope oldfiles<cr>", "File History"}
     map["tH"] = {"<cmd>Telescope help_tags<cr>", "Help"}
     map["tt"] = {"<cmd>Telescope tags<cr>", "Tags"}
     map["tl"] = {"<cmd>Telescope highlights<cr>", "Telescope"}
-    map["<space>"] = {"<cmd>Telescope buffers<cr>", "Buffers"}
+    map["tc"] = {"<cmd>Telescope command_history<cr>", "Recent Commands"}
+    map["tC"] = {"<cmd>Telescope commands<cr>", "Available Commands"}
+    map["t3"] = {"<cmd>Telescope search_history<cr>", "Search History"}
+    map["tq"] = {"<cmd>Telescope quickfix<cr>", "Quick Fix"}
+    map["tl"] = {"<cmd>Telescope loclist<cr>", "Location List"}
+    map["tj"] = {"<cmd>Telescope jumplist<cr>", "Jump List"}
+    map["t4"] = {"<cmd>Telescope spell_suggest", "Spell Suggest"}
+
+    -- Git
+    map["tb"] = {"<cmd>Telescope git_bcommits<cr>", "Buffer Commits"}
+    map["t1"] = {"<cmd>Telescope git_stash<cr>", "Git Stash"}
+    map["t2"] = {"<cmd>Telescope git_branches<cr>", "Git Branches"}
+
+
+    -- LSP
+    map["td"] = {"<cmd>Telescope lsp_document_symbols<cr>", "LSP document symbols"}
 end
 
 if vim.g.knob_toggleterm then
