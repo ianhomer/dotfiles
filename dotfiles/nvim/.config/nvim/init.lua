ca8f8110f025d457bc4583bb3d7c348270f8401e
Ian Homer <ian.homer@gmail.com>
2022-04-18 17:38:13 +0100

    feat: disable spell cap check

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -329,7 +329,7 @@ return require("packer").startup({
         use({
           'simrat39/symbols-outline.nvim',
           commit = "1361738c47892c3cee0d0b7a3b3bc7a8b48139c2",
-          config = [[require'config.symbols_outline']]
+          setup = [[require'config.symbols_outline']]
         })
 
         -- Navigation
