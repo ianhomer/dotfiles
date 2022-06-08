c1ceacb43d0a0dc5b7d9f4563e243d737fabe7ca
Ian Homer <ian@local>
2022-06-08 20:54:56 +0100

    feat: allow = in variable value

--- dotfiles/fish/.config/fish/functions/dotenv.fish
+++ dotfiles/fish/.config/fish/functions/dotenv.fish
@@ -11,7 +11,9 @@ function dotenv
     echo ".env file loaded : $envFile"
 
     for i in (grep -vh '^#' $envFile | awk 'NF')
-        set arr (echo $i |tr = \n)
+        echo $i
+        set arr (echo $i |sed 's/=/\n/')
+        echo $arr
         if [ $arr[1] = PATH ]
             set -gx $arr[1] $PATH $arr[2]
         else
