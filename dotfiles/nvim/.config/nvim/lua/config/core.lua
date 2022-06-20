717f8b0be8d06cb5f26d32ed79725f2b685b8bf1
Ian Homer <ian@local>
2022-06-20 08:51:43 +0100

    tmux and fold tuning

--- dotfiles/nvim/.config/nvim/lua/config/core.lua
+++ dotfiles/nvim/.config/nvim/lua/config/core.lua
@@ -2,38 +2,44 @@ local g = vim.g
 
 -- disable some builtin vim plugins
 local disabled_built_ins = {
-   "2html_plugin",
-   "getscript",
-   "getscriptPlugin",
-   "gzip",
-   "logipat",
-   "netrw",
-   "netrwPlugin",
-   "netrwSettings",
-   "netrwFileHandlers",
-   "matchit",
-   "tar",
-   "tarPlugin",
-   "rrhelper",
-   "spellfile_plugin",
-   "vimball",
-   "vimballPlugin",
-   "zip",
-   "zipPlugin",
+    "2html_plugin",
+    "getscript",
+    "getscriptPlugin",
+    "gzip",
+    "logipat",
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "matchit",
+    "tar",
+    "tarPlugin",
+    "rrhelper",
+    "spellfile_plugin",
+    "vimball",
+    "vimballPlugin",
+    "zip",
+    "zipPlugin",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-   g["loaded_" .. plugin] = 1
+    g["loaded_" .. plugin] = 1
 end
 
-vim.api.nvim_exec([[
+vim.api.nvim_exec(
+    [[
 command! -nargs=* Fuzzy Telescope grep_string search=<args>
-]], true)
+]],
+    true
+)
 
 -- Persistent undo file
 vim.opt.undofile = true
 -- Use HOME expansion instead of ~ - https://github.com/neovim/neovim/issues/15720
-vim.opt.undodir = os.getenv( "HOME" ) .. "/.vim/undo"
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undo"
 g.mundo_width = 60
 g.mundo_right = 1
 
+-- vim.o.foldcolumn = "2"
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
