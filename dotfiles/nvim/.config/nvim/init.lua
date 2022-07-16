0001b8c3f8eb5cecd699e5b0be900efbba86b7e6
Ian Homer <ian@local>
2022-07-16 11:42:13 +0100

    down: downgrade toggleterm plugin to July 12

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -608,7 +608,7 @@ return require("packer").startup({
             "akinsho/toggleterm.nvim",
             cmd = { "ToggleTerm", "TermExec" },
             config = [[require'config.toggleterm']],
-            commit = "91505816335a93b10f026409228aef5e8d48e6d9",
+            commit = "8e6f938ed8eec7f988dc07aec2af148ad57c6d95",
         })
         use({
             "tpope/vim-eunuch",
