f427802d3ddc993f122f8f2036860f0a909bd8f8
Ian Homer <code@ianhomer.com>
2022-08-31 21:45:11 +0100

    🔧remove normal mode insert

--- dotfiles/nvim/.config/nvim/lua/config/icon_picker.lua
+++ dotfiles/nvim/.config/nvim/lua/config/icon_picker.lua
@@ -4,5 +4,4 @@ require("icon-picker").setup({
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<Leader>I", "<cmd>IconPickerNormal emoji<cr>", opts)
 vim.keymap.set("i", "<C-l>", "<cmd>IconPickerInsert emoji<cr>", opts)
