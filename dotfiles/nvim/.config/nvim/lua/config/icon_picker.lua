3d79f7467173351195a51bc801a4680b9159a24f
Ian Homer <code@ianhomer.com>
2022-08-31 21:39:07 +0100

    🔧 icon select only emojis

--- dotfiles/nvim/.config/nvim/lua/config/icon_picker.lua
+++ dotfiles/nvim/.config/nvim/lua/config/icon_picker.lua
@@ -4,5 +4,5 @@ require("icon-picker").setup({
 
 local opts = { noremap = true, silent = true }
 
-vim.keymap.set("n", "<Leader>I", "<cmd>IconPickerNormal<cr>", opts)
-vim.keymap.set("i", "<C-l>", "<cmd>IconPickerInsert<cr>", opts)
+vim.keymap.set("n", "<Leader>I", "<cmd>IconPickerNormal emoji<cr>", opts)
+vim.keymap.set("i", "<C-l>", "<cmd>IconPickerInsert emoji<cr>", opts)
