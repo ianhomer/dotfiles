e2b55385fd0c57322c2a93f49f10085de029fb5a
Ian Homer <code@ianhomer.com>
2022-09-06 07:40:00 +0100

    🔧 nvim tree don't resize window on open

--- dotfiles/nvim/.config/nvim/lua/config/nvimtree.lua
+++ dotfiles/nvim/.config/nvim/lua/config/nvimtree.lua
@@ -45,6 +45,7 @@ require("nvim-tree").setup({
     },
     actions = {
         open_file = {
+            resize_window = false,
             window_picker = {
                 enable = true,
                 exclude = {
