8ad9c5f9ff5da822af8d0f723c4278db31d50118
Ian Homer <code@ianhomer.com>
2022-10-05 21:04:49 +0100

    improvve barbar mob mode toggle

--- dotfiles/nvim/.config/nvim/lua/config/barbar.lua
+++ dotfiles/nvim/.config/nvim/lua/config/barbar.lua
@@ -23,15 +23,34 @@ nvim_tree_events.subscribe("TreeClose", function()
     end
 end)
 
-require("bufferline").setup({
+-- Barbar only enabled in mob mode, so disable for now
+vim.o.showtabline = 0
+
+local M = {}
+
+local config = {
     animation = false,
-    auto_hide = false,
+    auto_hide = true,
+    icon_separator_active = "┃",
+    icon_separator_inactive = "┃",
     icon_pinned = "",
     closable = true,
-    icon_custom_colors = true,
-})
+    icon_custom_colors = false,
+}
 
--- Barbar only enabled in mob mode, so disable for now
-vim.o.showtabline=0
+function M.show()
+    config.auto_hide = true
+    require("bufferline").setup(config)
+    if (require("nvim-tree.view").is_visible()) then
+        bufferline_api.set_offset(get_tree_size())
+    else
+        bufferline_api.set_offset(0)
+    end
+end
 
+function M.hide()
+    config.auto_hide = false
+    require("bufferline").setup(config)
+end
 
+return M
