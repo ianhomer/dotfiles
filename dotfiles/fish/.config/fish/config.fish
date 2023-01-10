a1942106c8a0eb5e21179d3fc35892731e9ed294
Ian Homer <code@ianhomer.com>
2023-01-10 09:08:08 +0000

    python build chain tools

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -53,6 +53,9 @@ if status --is-login
     set --query nvm_current_version && _nvm_version_deactivate $nvm_current_version
     _nvm_version_activate $v
     time-me "AFTER nvm use"
+
+    # Enable pyenv
+    pyenv init - | source
 end
 
 if status --is-interactive
