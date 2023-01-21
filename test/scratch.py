baaeba02c7c90f22b606e92f4cb7f82a64d881dc
Ian Homer <code@ianhomer.com>
2023-01-21 16:28:36 +0000

    Update scratch.py playground

--- test/scratch.py
+++ test/scratch.py
@@ -4,11 +4,15 @@
 # Python scratch pad
 #
 
+from uuid import uuid4
+from scratch_module import FOO
 
 def foo(bar):
     print(f"foo executed with {bar}")
     return "value"
 
 
+print(FOO)
+print(uuid4())
 bar = "hello"
 foo(bar)
