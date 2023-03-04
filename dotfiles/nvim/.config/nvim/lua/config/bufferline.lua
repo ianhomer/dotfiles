59a1f8bdc76b4c87cca5d118436d33a47ed6c113
Ian Homer <code@ianhomer.com>
2023-03-04 16:09:28 +0000

    🔧 toggle tabline off by default

--- dotfiles/nvim/.config/nvim/lua/config/bufferline.lua
+++ dotfiles/nvim/.config/nvim/lua/config/bufferline.lua
@@ -1,7 +1,7 @@
 require("bufferline").setup({
   options = {
     diagnostics = "nvim_lsp",
-    always_show_bufferline = false,
+    always_show_bufferline = true,
     offsets = {
       {
         filetype = "NvimTree",
@@ -12,3 +12,7 @@ require("bufferline").setup({
     },
   },
 })
+-- I toggle tabline on in MobbingMode (with key map - <leader> 3) since tabline
+-- is normally on useful to me when I'm screen sharing and I'd like others to
+-- see files that are open
+vim.opt.showtabline = 0
