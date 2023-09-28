4be4cf374eea3c1486594758461264669a15172a
Ian Homer <code@ianhomer.com>
2023-09-28 21:15:05 +0100

    🔧 upgrade to indent blank line v3

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -129,15 +129,6 @@ nvim_set_var("knobs_layers_map", {
   },
 })
 
-g.indentLine_enabled = 1
-g.indent_blankline_char = "▏"
-
-g.indent_blankline_filetype_exclude = { "help", "startify", "terminal" }
-g.indent_blankline_buftype_exclude = { "terminal" }
-
-g.indent_blankline_show_trailing_blankline_indent = false
-g.indent_blankline_show_first_indent_level = false
-
 require("config.kitty")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -644,12 +635,27 @@ require("lazy").setup(
     {
       "lukas-reineke/indent-blankline.nvim",
       event = { "BufReadPost", "BufNewFile" },
+      main = "ibl",
       opts = {
-        char = "│",
-        filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble",
-          "lazy" },
-        show_trailing_blankline_indent = false,
-        show_current_context = false,
+        indent = {
+          char = "│",
+          highlight = "IndentBlanklineChar",
+        },
+        exclude = {
+          filetypes = {
+            "Trouble",
+            "alpha",
+            "dashboard",
+            "help",
+            "lazy",
+            "neo-tree",
+            "startify",
+            "terminal",
+          },
+          buftypes = {
+            "terminal"
+          }
+        },
       },
     },
     {
