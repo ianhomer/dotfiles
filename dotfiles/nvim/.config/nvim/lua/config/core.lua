f862655e4d98c398d1b2d0a69c6125c09970f8bf
Ian Homer <code@ianhomer.com>
2023-12-09 09:27:22 +0000

    nvim image support ; generic gdrive backup

--- dotfiles/nvim/.config/nvim/lua/config/core.lua
+++ dotfiles/nvim/.config/nvim/lua/config/core.lua
@@ -24,6 +24,8 @@ for _, plugin in pairs(disabled_built_ins) do
     vim.g["loaded_" .. plugin] = 1
 end
 
+
+
 vim.api.nvim_exec(
     [[
 command! -nargs=* Fuzzy execute "Telescope grep_string search=".escape(<q-args>, ' ')
