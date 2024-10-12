cbe4ce327ce7021dc652680fb9f91369543ce5d1
Ian Homer <code@ianhomer.com>
2024-10-12 09:50:00 +0100

    🔧 Shortcuts for fugitive git experience

--- dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,30 +1,42 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  opts = function()
-    local actions = require("telescope.actions")
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-telescope/telescope-symbols.nvim" },
+    opts = function()
+      local actions = require("telescope.actions")
 
-    return {
-      defaults = {
-        mappings = {
-          i = {
-            ["<esc>"] = actions.close,
+      return {
+        defaults = {
+          mappings = {
+            i = {
+              ["<esc>"] = actions.close,
+            },
           },
+          layout_strategy = "vertical",
+          layout_config = {
+            horizontal = {
+              prompt_position = "bottom",
+            },
+            vertical = {
+              mirror = false,
+            },
+            height = 0.99,
+            width = 0.99,
+          },
+          winblend = 0,
+          border = {},
+          borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
         },
-        layout_strategy = "vertical",
-        layout_config = {
-          horizontal = {
-            prompt_position = "bottom",
-          },
-          vertical = {
-            mirror = false,
-          },
-          height = 0.99,
-          width = 0.99,
-        },
-        winblend = 0,
-        border = {},
-        borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+      }
+    end,
+
+    keys = {
+      {
+        "<c-l>",
+        mode = "i",
+        "<cmd>lua require'telescope.builtin'.symbols{ sources = {'gitmoji'}}<cr>",
       },
-    }
-  end,
+    },
+  },
+  { "nvim-telescope/telescope-symbols.nvim" },
 }
