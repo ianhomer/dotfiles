d0193c7ba84aff1aa1cc5374acb6e8fae2de43ff
Ian Homer <code@ianhomer.com>
2024-10-14 16:11:40 +0100

    🔧 move vim spelling to nvim
    nvim is now default editor, vim backup and spelling in vim is not essential.
    Better now to go with the defaults of nvim

--- dotfiles/nvim/.config/nvim/lua/config/options.lua
+++ dotfiles/nvim/.config/nvim/lua/config/options.lua
@@ -8,4 +8,16 @@ vim.g.autoformat = false
 local opt = vim.opt
 opt.relativenumber = false
 opt.conceallevel = 0
-opt.spellfile = vim.fn.expand("$HOME/.config/vim/spell/en.utf-8.add")
+
+-- Shift + J/K moves selected lines down/up in visual mode
+local cmd = vim.cmd
+cmd([[
+vnoremap <silent> J :m '>+1<CR>gv=gv
+vnoremap <silent> K :m '<-2<CR>gv=gv
+vnoremap <silent> <S-Up> :m '<-2<CR>gv=gv
+vnoremap <silent> <S-Down> :m '>+1<CR>gv=gv
+nnoremap <silent> <S-Up> :m-2<CR>
+nnoremap <silent> <S-Down> :m+<CR>
+inoremap <silent> <S-Up> <Esc>:m-2<CR>
+inoremap <silent> <S-Down> <Esc>:m+<CR>
+  ]])
--- dotfiles/nvim/.config/nvim/lua/plugins/fugitive.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "tpope/vim-fugitive",
-  lazy = false,
-  keys = {
-    { "<leader>gg", "<cmd>Git<cr>", desc = "Fugitive" },
-    { "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
-  },
-}
