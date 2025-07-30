4a12ee0dec173a567dc518b7fd7cce904480e7ca
Ian Homer <code@ianhomer.com>
2025-07-30 11:45:22 +0100

    fix: provide pre-rsync for git-synk

---
+++ dotfiles/nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,27 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      -- See https://github.com/LazyVim/LazyVim/issues/3383
+      eslint = {
+        settings = {
+          useFlatConfig = false, -- set if using flat config
+          experimental = {
+            useFlatConfig = nil, -- option not in the latest eslint-lsp
+          },
+        },
+      },
+    },
+    setup = {
+      eslint = function()
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" or client.name == "vtsls" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
+      end,
+    },
+  },
+}
